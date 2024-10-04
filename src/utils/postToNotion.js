5dc474d573f3bdd23dbad5d908d7956cae649009
gdcho <gcho13@my.bcit.ca>
2024-10-03 23:10:49 -0700

    fix: set max summary length to fetch

--- src/utils/postToNotion.js
+++ src/utils/postToNotion.js
@@ -1,80 +1,85 @@
 async function postToNotion(bookDetails, notionToken, notionDatabaseId) {
-  const authorsArray = Array.isArray(bookDetails.authors)
-    ? bookDetails.authors
-    : [bookDetails.authors].filter(Boolean);
+    const authorsArray = Array.isArray(bookDetails.authors)
+        ? bookDetails.authors
+        : [bookDetails.authors].filter(Boolean);
 
-  const bodyData = {
-    parent: { database_id: notionDatabaseId },
-    icon: {
-      emoji: "📖",
-    },
-    cover: {
-      external: {
-        url: bookDetails.imageUrl,
-      },
-    },
-    properties: {
-      Title: {
-        title: [
-          {
-            text: {
-              content: bookDetails.title,
-            },
-          },
-        ],
-      },
-      Author: {
-        rich_text: [
-          {
-            text: {
-              content: authorsArray.join(", "),
-            },
-          },
-        ],
-      },
-      Link: {
-        url: bookDetails.link,
-      },
-      "Total pages": {
-        number: parseInt(bookDetails.totalPages, 10),
-      },
-      "Current page": {
-        number: 0,
-      },
-      Summary: {
-        rich_text: [
-          {
-            text: {
-              content: bookDetails.summary,
-            },
-          },
-        ],
-      },
-    },
-  };
+    const MAX_SUMMARY_LENGTH = 2000;
 
-  const response = await fetch(`https://api.notion.com/v1/pages`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${notionToken}`,
-      "Content-Type": "application/json",
-      "Notion-Version": "2021-08-16",
-    },
-    body: JSON.stringify(bodyData),
-  });
+    const bodyData = {
+        parent: { database_id: notionDatabaseId },
+        icon: {
+            emoji: "📖",
+        },
+        cover: {
+            external: {
+                url: bookDetails.imageUrl,
+            },
+        },
+        properties: {
+            Title: {
+                title: [
+                    {
+                        text: {
+                            content: bookDetails.title,
+                        },
+                    },
+                ],
+            },
+            Author: {
+                rich_text: [
+                    {
+                        text: {
+                            content: authorsArray.join(", "),
+                        },
+                    },
+                ],
+            },
+            Link: {
+                url: bookDetails.link,
+            },
+            "Total pages": {
+                number: parseInt(bookDetails.totalPages, 10),
+            },
+            "Current page": {
+                number: 0,
+            },
+            Summary: {
+                rich_text: [
+                    {
+                        text: {
+                            content: bookDetails.summary.slice(
+                                0,
+                                MAX_SUMMARY_LENGTH,
+                            ),
+                        },
+                    },
+                ],
+            },
+        },
+    };
 
-  try {
-    if (response.ok) {
-      const responseData = await response.json();
-      return responseData;
-    } else {
-      const textResponse = await response.text();
-      throw new Error(`Failed to post to Notion: ${textResponse}`);
+    const response = await fetch(`https://api.notion.com/v1/pages`, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${notionToken}`,
+            "Content-Type": "application/json",
+            "Notion-Version": "2021-08-16",
+        },
+        body: JSON.stringify(bodyData),
+    });
+
+    try {
+        if (response.ok) {
+            const responseData = await response.json();
+            return responseData;
+        } else {
+            const textResponse = await response.text();
+            throw new Error(`Failed to post to Notion: ${textResponse}`);
+        }
+    } catch (error) {
+        console.error("Failed to parse response as JSON:", error);
+        throw new Error("Failed to process the response");
     }
-  } catch (error) {
-    console.error("Failed to parse response as JSON:", error);
-    throw new Error("Failed to process the response");
-  }
 }
 
 export default postToNotion;
